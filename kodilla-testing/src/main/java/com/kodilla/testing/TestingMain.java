c6cf7e319f6fedbca8173b2d2837f4dd316c26e3
apagabrys <apagabrys@gmail.com>
2017-09-08 20:30:59 +0200

    Add test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        Calculator calculator  = new Calculator();
+            int sum = calculator.addAToB(5,5);
+            int divide = calculator.substractAFromB(192,41);
+            System.out.println("a + b = " + sum);
+            System.out.println("c - d = " + divide);
+
     }
 }
