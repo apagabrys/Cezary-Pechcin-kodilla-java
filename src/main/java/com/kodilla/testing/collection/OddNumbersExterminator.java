f64b8e0d200174d21e6db16a9672cadc7d60f0cb
apagabrys <nasz_email_do_github>
2017-11-03 17:19:37 +0100

    Exercise is working

--- src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -9,7 +9,9 @@ public class OddNumbersExterminator {
     public ArrayList<Integer> exterminate(ArrayList<Integer> numbers){
         ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
         for(int i:numbers) {
-            evenNumbers.add(i);
+            if (i % 2 == 0) {
+                evenNumbers.add(i);
+            }
         }
         return evenNumbers;
     }
