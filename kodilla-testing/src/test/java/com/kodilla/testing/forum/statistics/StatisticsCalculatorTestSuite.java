4b09cdb2af521f5ced9e8e7bd5c0bc249a0675ce
apagabrys <nasz_email_do_github>
2017-11-23 11:40:50 +0100

    6.5 wszystkie testy

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsCalculatorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsCalculatorTestSuite.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.Mockito.mock;
@@ -11,7 +12,7 @@ import static org.mockito.Mockito.when;
 
 public class StatisticsCalculatorTestSuite {
     @Test
-    public void test1000Posts(){
+    public void test1000Posts0Comments(){
         //Given
         Statistics statisticsMock = mock (Statistics.class);
         List<String> users = new ArrayList<String>();
@@ -33,4 +34,75 @@ public class StatisticsCalculatorTestSuite {
         Assert.assertEquals(0.0, statisticsCalculator.getAverageCommentPerUser(), 0.01);
         Assert.assertEquals(0.0, statisticsCalculator.getAverageCommentPerPost(), 0.01);
     }
+    @Test
+    public void test0Posts(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        int postsCounter = 0;
+        when(statisticsMock.postsCount()).thenReturn(postsCounter);
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator();
+
+        //When
+        statisticsCalculator.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertEquals(0.0, statisticsCalculator.getAveragePostPerUser(), 0.01);
+    }
+    @Test
+    public void testCommentsGreaterThanPosts(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        int postsCounter = 1;
+        int commentsCounter = 2;
+        when(statisticsMock.postsCount()).thenReturn(postsCounter);
+        when(statisticsMock.commentsCount()).thenReturn(commentsCounter);
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator();
+        //When
+        statisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(2.0, statisticsCalculator.getAverageCommentPerPost(), 0.01);
+    }
+    @Test
+    public void testPostsGreaterThanComments(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        int postsCounter = 2;
+        int commentsCounter = 1;
+        when(statisticsMock.postsCount()).thenReturn(postsCounter);
+        when(statisticsMock.commentsCount()).thenReturn(commentsCounter);
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator();
+        //When
+        statisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(0.5, statisticsCalculator.getAverageCommentPerPost(), 0.01);
+    }
+    @Test
+    public void test0Users(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        List<String> list = Collections.nCopies(1000, "Czarek");
+        int postCounter = 1000;
+        when(statisticsMock.userNames()).thenReturn(list);
+        when(statisticsMock.postsCount()).thenReturn(postCounter);
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator();
+        //When
+        statisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(1.0, statisticsCalculator.getAveragePostPerUser(), 0.01);
+    }
+    @Test
+    public void test1000Users(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        List<String> list = Collections.emptyList();
+        int postCounter = 0;
+        when(statisticsMock.userNames()).thenReturn(list);
+        when(statisticsMock.postsCount()).thenReturn(postCounter);
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator();
+        //When
+        statisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(0.0, statisticsCalculator.getAveragePostPerUser(), 0.01);
+    }
 }
+
